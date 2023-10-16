60e6cc7491112289b35fe1d8f62e8b2010723a3b
Andrewij21 <andrewijaya988@gmail.com>
2023-10-16 14:56:08 +0700

    feat: add device path

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layouts/Layout";
 import Home from "./page/Home";
+import Devices from "./page/Devices";
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/devices" element={<Devices />} />
         </Route>
       </Routes>
     </>
