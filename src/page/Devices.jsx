ed9565eba6fec1a5c213e970a5847704fdf3d579
Andrewij21 <andrewijaya988@gmail.com>
2023-10-16 21:48:29 +0700

    feat: show modal in device page

--- src/page/Devices.jsx
+++ src/page/Devices.jsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
+import Modal from "../components/ui/Modal";
 import Table from "../components/ui/Table";
 import { BsPlus } from "react-icons/bs";
 
 const Devices = () => {
+  const [showModal, setShowModal] = useState(false);
+  const toggleModel = () => {
+    setShowModal(!showModal);
+  };
+
   return (
     <div>
       <h1 className="text-2xl text-sky-800 font-bold capitalize">Devices</h1>
       <div className="flex w-full justify-end items-center my-4">
-        <button className="flex gap-1 flex-row items-center bg-sky-400 px-4 py-2 rounded-lg hover:bg-sky-500 hover:ring-sky-400 hover:ring-2 ">
+        <button
+          onClick={toggleModel}
+          className="flex gap-1 flex-row items-center bg-teal-400 px-4 py-2 rounded-lg hover:bg-teal-500 hover:ring-teal-400 hover:ring-2 text-white"
+        >
           <BsPlus className="font-bold text-xl" />
           <span className="capitalize text-sm font-semibold">add Device</span>
         </button>
       </div>
+      {showModal ? (
+        <Modal toggleModel={toggleModel} title={"add device"} />
+      ) : null}
       <Table />
     </div>
   );
