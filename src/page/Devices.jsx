8aa035f73d8623788883e58ceb9b15c85e13c6cc
Andrewij21 <andrewijaya988@gmail.com>
2023-10-16 19:49:18 +0700

    feat: add button

--- src/page/Devices.jsx
+++ src/page/Devices.jsx
@@ -1,8 +1,16 @@
 import Table from "../components/ui/Table";
+import { BsPlus } from "react-icons/bs";
 
 const Devices = () => {
   return (
     <div>
+      <h1 className="text-2xl text-sky-800 font-bold capitalize">Devices</h1>
+      <div className="flex w-full justify-end items-center my-4">
+        <button className="flex gap-1 flex-row items-center bg-sky-400 px-4 py-2 rounded-lg hover:bg-sky-500 hover:ring-sky-400 hover:ring-2 ">
+          <BsPlus className="font-bold text-xl" />
+          <span className="capitalize text-sm font-semibold">add Device</span>
+        </button>
+      </div>
       <Table />
     </div>
   );
