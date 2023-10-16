3379f009523aec6441f92ded41325ea36db1c6c8
Andrewij21 <andrewijaya988@gmail.com>
2023-10-16 13:33:46 +0700

    refactor: turn on animation

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-// import { motion } from "framer-motion";
+import { motion } from "framer-motion";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 
-// const variants = {
-//   expanded: { width: "20%" },
-//   nonexpanded: { width: "10%" },
-// };
+const variants = {
+  expanded: { width: "20%" },
+  nonexpanded: { width: "10%" },
+};
 
 const Layout = () => {
   const [openMenu, setOpenmenu] = useState(false);
@@ -17,13 +17,13 @@ const Layout = () => {
 
   return (
     <div className="w-full flex">
-      <div
-        // animate={openMenu ? "expanded" : "nonexpanded"}
-        // variants={variants}
+      <motion.div
+        animate={openMenu ? "expanded" : "nonexpanded"}
+        variants={variants}
         className={`${openMenu ? "w-[20%]" : "w-[10%]"} hidden md:block`}
       >
         <Sidebar menu={openMenu} />
-      </div>
+      </motion.div>
       <div className="w-screen min-h-screen">
         <Navbar action={menuHandler} />
         <main>
