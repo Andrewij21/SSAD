5509cde69505379acb0ae88b67456ee9909dc8e0
Andrewij21 <andrewijaya988@gmail.com>
2023-10-21 09:25:13 +0700

    refactor: add bg gradient

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,6 +1,6 @@
 const Login = () => {
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="bg-gradient-to-tl from-blue-600 to-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a
           href="#"
