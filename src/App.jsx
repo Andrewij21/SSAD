c192bae5439bbe6a77a35379240cc88c57350108
Andrewij21 <andrewijaya988@gmail.com>
2023-10-21 08:58:07 +0700

    feat: add login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Layout from "./components/layouts/Layout";
 import Home from "./page/Home";
 import Devices from "./page/Devices";
 import Personeles from "./page/Personels";
+import Login from "./page/Login";
 function App() {
   return (
     <>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/devices" element={<Devices />} />
