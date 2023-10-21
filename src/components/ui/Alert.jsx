03514e8b8bf169c093eaf49242cfb48e280d40ab
Andrewij21 <andrewijaya988@gmail.com>
2023-10-21 12:07:05 +0700

    feat: dynamic msg and center card

--- src/components/ui/Alert.jsx
+++ src/components/ui/Alert.jsx
@@ -1,4 +1,5 @@
 import { motion } from "framer-motion";
+import PropTypes from "prop-types";
 
 const dropIn = {
   hidden: {
@@ -21,20 +22,22 @@ const dropIn = {
   },
 };
 
-const Alert = () => {
+const Alert = ({ msg }) => {
   return (
     <motion.div
-      className="absolute left-1/3 p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
+      className="absolute right-1/3 left-1/3 top-12 p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400"
       role="alert"
       variants={dropIn}
       initial="hidden"
       animate="visible"
       exit="exit"
     >
-      <span className="font-medium">Danger alert!</span> Change a few things up
-      and try submitting again.
+      <span className="font-medium">{msg}</span>
     </motion.div>
   );
 };
 
+Alert.propTypes = {
+  msg: PropTypes.string,
+};
 export default Alert;
