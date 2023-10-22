238394225f26eaf0672a15de1fa55214088c1a2d
Andrewij21 <andrewijaya988@gmail.com>
2023-10-22 14:53:18 +0700

    feat: w can clamp

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        clamp: "(50%,700px,90%)",
+      },
+    },
   },
   plugins: [],
 };
