054c7785bb4fc9e79084f48f2a61a3edfd95ed9b
Andrewij21 <andrewijaya988@gmail.com>
2023-10-22 14:56:11 +0700

    refactor: adjust scale

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -29,8 +29,8 @@ const Home = () => {
         {cards.map((card, i) => {
           return (
             <motion.div
-              whileHover={{ scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
+              whileHover={{ scale: 1.05 }}
+              whileTap={{ scale: 0.95 }}
               transition={{
                 type: "spring",
                 stiffness: 200,
