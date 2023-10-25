3c09251027787ea579b6dc2eb2a0a6cb8d0dc026
Andrewij21 <andrewijaya988@gmail.com>
2023-10-25 12:52:33 +0700

    refactor: modify it so it not logout when refresh

--- src/page/RequireAuth.jsx
+++ src/page/RequireAuth.jsx
@@ -1,12 +1,38 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
+import useRefreshToken from "../hooks/useRefreshToken";
 
 const RequireAuth = () => {
   const { auth } = useAuth();
-  return <>{auth.username ? <Outlet /> : <Navigate to="/login" replace />}</>;
-};
+  const [isLoading, setIsLoading] = useState(true);
+  const refresh = useRefreshToken();
 
+  useEffect(() => {
+    let isMounted = true;
+    const verifyRefreshToken = async () => {
+      try {
+        await refresh();
+      } catch (error) {
+        console.error(error);
+      } finally {
+        isMounted && setIsLoading(false);
+      }
+    };
+    !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
+    // !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
+    return () => (isMounted = false);
+  }, [auth, refresh]);
+
+  // const { auth } = useAuth();
+  // return <>{auth.username ? <Outlet /> : <Navigate to="/login" replace />}</>;
+  return (
+    <>
+      {auth.username ? <Outlet /> : isLoading ? <p>loading...</p> : <Outlet />}
+    </>
+  );
+};
 RequireAuth.propTypes = {
   allowedRoles: PropTypes.array,
 };
