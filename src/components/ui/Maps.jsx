9f28e866ccd3b0fdd9078af40e2d364c056266eb
Andrewij21 <andrewijaya988@gmail.com>
2023-11-03 15:36:36 +0700

    feat: display dynamic marker location

--- src/components/ui/Maps.jsx
+++ src/components/ui/Maps.jsx
@@ -1,12 +1,13 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import propTypes from "prop-types";
 import "leaflet/dist/leaflet.css";
 
-export default function Map() {
+export default function Map({ marker }) {
   return (
     // Important! Always set the container height explicitly
     <div className="">
       <MapContainer
-        center={[51.505, -0.09]}
+        center={[-5.37775571957218, 105.2521859368718]}
         zoom={13}
         scrollWheelZoom={false}
         className="h-screen"
@@ -15,12 +16,24 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
+        {marker &&
+          marker.map((mark) => {
+            return (
+              <Marker key={mark._id} position={mark.area.latLong}>
+                <Popup>{mark.name}</Popup>
+              </Marker>
+            );
+          })}
+        {/* <Marker position={[-5.37775571957218, 105.2521859368718]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
-        </Marker>
+        </Marker> */}
       </MapContainer>
     </div>
   );
 }
+
+Map.propTypes = {
+  marker: propTypes.array,
+};
