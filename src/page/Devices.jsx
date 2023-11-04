9a1430a7695cd2b8f5f20ef155eccbe958c54d2b
Andrewij21 <andrewijaya988@gmail.com>
2023-11-04 20:04:52 +0700

    refactor: change search style

--- src/page/Devices.jsx
+++ src/page/Devices.jsx
@@ -125,7 +125,7 @@ const Devices = () => {
   return (
     <div>
       <h1 className="text-2xl text-sky-600 font-bold capitalize">Devices</h1>
-      <div className="flex w-full justify-between items-center my-4">
+      <div className="md:flex w-full justify-between items-center my-4 space-y-4">
         <div className="w-full md:w-1/2">
           <form className="flex items-center">
             <div className="relative w-full">
