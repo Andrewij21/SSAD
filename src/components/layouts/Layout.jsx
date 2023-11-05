0ab2ccd48fd2f4c267db316ff586dd9e5a79a2d7
Andrewij21 <andrewijaya988@gmail.com>
2023-11-05 15:52:53 +0700

    refactor: add props isMobile to sidebar

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -26,7 +26,7 @@ const Layout = () => {
         variants={variants}
         className={`hidden md:block`}
       >
-        <Sidebar menu={openMenu} />
+        <Sidebar menu={openMenu} isMobile={false} />
       </motion.div>
       <motion.div
         // animate={openMenu ? "expanded" : "nonexpanded"}
@@ -35,10 +35,10 @@ const Layout = () => {
           openMenu ? "left-0" : " -right-full"
         } fixed top-0 h-screen z-50 md:hidden w-36`}
       >
-        <Sidebar menu={openMenu} />
+        <Sidebar menu={openMenu} isMobile={true} toggleMenu={menuHandler} />
       </motion.div>
 
-      <div className="w-screen" onClick={() => setOpenmenu(!openMenu)}>
+      <div className="w-screen">
         <Navbar action={menuHandler} />
         <main className="px-6 py-4 min-h-[calc(100vh-64px)] bg-slate-200">
           <Outlet />
