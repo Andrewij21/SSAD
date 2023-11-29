2fe960cb017e1845128f5fb67b91249b1e7241a0
andrewijaya <andrewijaya988@gmail.com>
2023-11-29 11:48:32 +0700

    refactor: change data props to object

--- src/components/ui/Details.jsx
+++ src/components/ui/Details.jsx
@@ -1,40 +1,44 @@
 import propTypes from "prop-types";
 
-const RAK = [{ name: "RAK-1", value: "rak1" }, { name: "RAK-2", value: "rak2" }, { name: "RAK-3", value: "rak3" }]
+const RAK = [
+  { name: "RAK-1", value: "rak1" },
+  { name: "RAK-2", value: "rak2" },
+  { name: "RAK-3", value: "rak3" },
+];
 
 const Details = ({ data }) => {
-    // console.log("detail component", { data })
-    const unit = data[0].set;
-    return (
-        <div className="space-y-6">
-            {
-                RAK.map((rak, i) => {
-                    return <div key={i}>
-                        <h4 className="text-lg font-semibold">{rak.name}</h4>
-                        {/* {data.map((set, i) => {
+  //   console.log("detail component", { data });
+  const unit = data.set;
+  return (
+    <div className="space-y-6">
+      {RAK.map((rak, i) => {
+        return (
+          <div key={i}>
+            <h4 className="text-lg font-semibold">{rak.name}</h4>
+            {/* {data.map((set, i) => {
                             return <> */}
-                        <div className="flex items-center justify-start space-x-2" key={i}>
-                            <p className="w-28">tanaman</p>
-                            <p>:</p>
-                            <span>{unit ? data[0]?.set[rak.value]?.tanaman : ""}</span>
-                        </div>
-                        <div className="flex items-center justify-start space-x-2">
-                            <p className="w-28">Media tumbuh</p>
-                            <p>:</p>
-                            <span>{unit ? data[0].set[rak.value]?.mediaTumbuh : ""}</span>
-                        </div>
-                        {/* </> */}
+            <div className="flex items-center justify-start space-x-2" key={i}>
+              <p className="w-28">tanaman</p>
+              <p>:</p>
+              <span>{unit ? unit[rak.value]?.tanaman : ""}</span>
+            </div>
+            <div className="flex items-center justify-start space-x-2">
+              <p className="w-28">Media tumbuh</p>
+              <p>:</p>
+              <span>{unit ? unit[rak.value]?.mediaTumbuh : ""}</span>
+            </div>
+            {/* </> */}
 
-                        {/* })} */}
-                    </div>
-                })
-            }
-        </div>
-    )
-}
+            {/* })} */}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
 Details.propTypes = {
-    data: propTypes.array,
-}
+  data: propTypes.object,
+};
 
 export default Details;
