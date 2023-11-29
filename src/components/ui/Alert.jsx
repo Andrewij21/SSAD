40e59b80f0cc1747a782d8af8af9073030f15651
andrewijaya <andrewijaya988@gmail.com>
2023-11-29 17:44:26 +0700

    feat: alert accept func and data

--- src/components/ui/Alert.jsx
+++ src/components/ui/Alert.jsx
@@ -31,7 +31,9 @@ const types = {
   confirm: { name: "confirm", color: "bg-slate-50 text-black" },
 };
 
-const Alert = ({ type, msg, title }) => {
+const Alert = ({ type, msg, title, handler, data }) => {
+  // const [btnValue, setBtnValue] = useState(false);
+  const btnHandler = (payload) => handler(payload, data);
   return (
     <motion.div
       className={`absolute right-1/3 left-1/3 top-12 p-4 mb-4 text-sm ${types[type].color} rounded-lg dark:bg-gray-800 dark:text-red-400 z-10`}
@@ -47,10 +49,16 @@ const Alert = ({ type, msg, title }) => {
       </p>
       {type === "confirm" && (
         <div className="text-right space-x-4">
-          <button className="px-6 py-2 rounded-lg text-sm text-white bg-rose-600 hover:bg-red-500 hover:ring-2 hover:ring-rose-500">
+          <button
+            className="px-6 py-2 rounded-lg text-sm text-white bg-rose-600 hover:bg-red-500 hover:ring-2 hover:ring-rose-500"
+            onClick={() => btnHandler(true)}
+          >
             Ok
           </button>
-          <button className="px-4 py-2 rounded-lg text-sm text-white bg-gray-400 hover:bg-gray-300 hover:ring-2 hover:ring-gray-300">
+          <button
+            className="px-4 py-2 rounded-lg text-sm text-white bg-gray-400 hover:bg-gray-300 hover:ring-2 hover:ring-gray-300"
+            onClick={() => btnHandler(false)}
+          >
             Cancle
           </button>
         </div>
@@ -63,5 +71,7 @@ Alert.propTypes = {
   msg: PropTypes.string,
   type: PropTypes.string,
   title: PropTypes.string,
+  handler: PropTypes.func,
+  data: PropTypes.object,
 };
 export default Alert;
