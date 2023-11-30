3463dd20069d205b2752432b1f40591b7e37d08c
andrewijaya <andrewijaya988@gmail.com>
2023-11-30 14:43:20 +0700

    feat: add select element

--- src/components/ui/Form.jsx
+++ src/components/ui/Form.jsx
@@ -30,31 +30,58 @@ const Form = ({ submitHandler, fields, isLoading, error, data }) => {
             >
               {field.label}
             </label>
-            <input
-              type={field.type}
-              name={field.name}
-              id={field.name}
-              {...register(`${field.name}`, {
-                required: {
-                  value: field.required,
-                  message: field.label + " is required",
-                },
-                pattern: {
-                  value: /^[^\s]+(?:$|.*[^\s]+$)/,
-                  message: "Entered value cant start/end with white spacing",
-                },
-              })}
-              className={`${
-                field.type == "text" ? "block w-full" : "align-middle ml-2"
-              } bg-gray-50 border border-gray-300 outline-none text-gray-900 sm:text-sm rounded-lg focus:ring-sky-600 focus:border-sky-600 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
-              placeholder={field.placeholder || ""}
-            />
+            {field.type !== `select` ? (
+              <input
+                type={field.type}
+                name={field.name}
+                id={field.name}
+                {...register(`${field.name}`, {
+                  required: {
+                    value: field.required,
+                    message: field.label + " is required",
+                  },
+                  pattern: {
+                    value: /^[^\s]+(?:$|.*[^\s]+$)/,
+                    message: "Entered value cant start/end with white spacing",
+                  },
+                })}
+                className={`${
+                  field.type == "text" ? "block w-full" : "align-middle ml-2"
+                } bg-gray-50 border border-gray-300 outline-none text-gray-900 sm:text-sm rounded-lg focus:ring-sky-600 focus:border-sky-600 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+                placeholder={field.placeholder || ""}
+              />
+            ) : (
+              <select
+                type={field.type}
+                name={field.name}
+                id={field.name}
+                {...register(`${field.name}`, {
+                  required: {
+                    value: field.required,
+                    message: field.label + " is required",
+                  },
+                  pattern: {
+                    value: /^[^\s]+(?:$|.*[^\s]+$)/,
+                    message: "Entered value cant start/end with white spacing",
+                  },
+                })}
+                className="bg-gray-50 border border-gray-300 outline-none text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option defaultValue="user">User</option>
+                <option value="admin">Admin</option>
+              </select>
+            )}
+
             <p className="text-pink-600 lowercase text-sm">
               {errors[field.name]?.message}
             </p>
           </div>
         );
       })}
+      {/* <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        Select Role
+      </label> */}
+
       <div className="flex items-center justify-center md:justify-end p-6 border-t border-solid border-blueGray-200 rounded-b">
         {isLoading ? (
           <Spinners />
