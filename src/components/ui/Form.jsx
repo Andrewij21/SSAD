13e09418c02c71caac70860991e7e7736b6a5ab3
andrewijaya <andrewijaya988@gmail.com>
2023-12-01 22:38:34 +0700

    refactor: change to lowercase

--- src/components/ui/Form.jsx
+++ src/components/ui/Form.jsx
@@ -67,8 +67,8 @@ const Form = ({ submitHandler, fields, isLoading, error, data }) => {
                 })}
                 className="bg-gray-50 border border-gray-300 outline-none text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               >
-                <option defaultValue="user">User</option>
-                <option value="admin">Admin</option>
+                <option defaultValue="user">user</option>
+                <option value="admin">admin</option>
               </select>
             )}
 
